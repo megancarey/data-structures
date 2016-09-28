6c27efbbe17dabe8449a251beab71dbc6eed4a0b
Megan Carey <mcarey@berkeley.edu>
2016-09-28 15:38:21 -0700

    Update FixedHashMap.java

--- FixedHashMap.java
+++ FixedHashMap.java
@@ -1,17 +1,5 @@
 /**
- * KPCB Coding Challenge Submission: Megan Carey
  * @author mcarey
- * 
- * Problem statement:
- * Using only primitive types, implement a fixed-size hash map that 
- * associates string keys with arbitrary data object references (you
- * don't need to copy the object). Your data structure should be 
- * optimized for algorithmic runtime and memory usage. You should 
- * not import any external libraries, and may not use primitive hash 
- * map or dictionary types in languages like Python or Ruby.
- *
- * Use only primitive types: boolean, byte, char, short, int, long,
- * float, and double.
  **/
 
 public class FixedHashMap {
